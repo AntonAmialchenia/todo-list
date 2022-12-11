e20195c01dcd22ee1cd08c1084d4fb0584d05048
Антон Амельченя <107065275+AntonAmialchenia@users.noreply.github.com>
2022-12-11 19:26:57 +0300

    Структура и компоненты

---
+++ src/components/Profile.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div className="content__image">
+        <img
+          src="https://greenviewchurch.co.uk/wp-content/uploads/2019/05/Busyness-title-image-1600x400.png"
+          alt="content"
+        />
+      </div>
+      <div>ava + description</div>
+      <div>
+        My posts
+        <div>Mew post</div>
+      </div>
+      <div>
+        <div>post 1</div>
+        <div>post 2</div>
+        <div>post 3</div>
+        <div>post 4</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
