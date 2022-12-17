599f01794b8ae71440b8105f7b6cdc7f28f0d3ac
Антон Амельченя <107065275+AntonAmialchenia@users.noreply.github.com>
2022-12-17 12:34:01 +0300

    Роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,22 @@ import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div className="app-wrapper__content">
-        <Dialogs />
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper__content">
+          <Routes>
+            <Route path="/dialogs" element={<Dialogs />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </div>
       </div>
-      {/* <Profile /> */}
-      
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import cls from "./Navbar.module.css";
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav className={cls.nav}>
       <div className={cls.item}>
-        <a>Profile</a>
+        <Link to='/profile' className={cls.link}>Profile</Link>
+        {/* <a className={cls.link} href="/profile">Profile</a> */}
       </div>
       <div className={cls.item}>
-        <a>Messages</a>
+      <Link to='/dialogs' className={cls.link}>Messages</Link>
+        {/* <a className={cls.link} href="/dialogs">Messages</a> */}
       </div>
       <div className={cls.item}>
-        <a>Mews</a>
+        <a className={cls.link}>Mews</a>
       </div>
       <div className={cls.item}>
-        <a>Music</a>
+        <a className={cls.link}>Music</a>
       </div>
       <div className={cls.item}>
-        <a>Settings</a>
+        <a className={cls.link}>Settings</a>
       </div>
     </nav>
   );
