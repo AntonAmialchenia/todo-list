bb2552cf0c2abe98f2a1374bcd7807f596d755a7
Антон Амельченя <107065275+AntonAmialchenia@users.noreply.github.com>
2022-12-17 16:12:31 +0300

    Dialogs
    Верстка страны dialogs, красоту навел в общем

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import cls from './Dialogs.module.css'
+import cls from './Dialogs.module.css';
 
 const Dialogs = () => {
-    return (
-        <div>
-            Dialogsssdf
-        </div>
-    );
+  return (
+    <div className={cls.dialogs}>
+      <div className={cls.dialogs__items}>
+        <div className={cls.dialogs__item + ' ' + cls.active}>Anton</div>
+        <div className={cls.dialogs__item}>Andrei</div>
+        <div className={cls.dialogs__item}>Sergei</div>
+        <div className={cls.dialogs__item}>Petr</div>
+        <div className={cls.dialogs__item}>Ivan</div>
+        <div className={cls.dialogs__item}>Dima</div>
+      </div>
+      <div className={cls.messages}>
+        <div className={cls.messages__item}>Hi</div>
+        <div className={cls.messages__item}>Hello!</div>
+        <div className={cls.messages__item}>Yo</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,26 +1,49 @@
-import React from "react";
-import cls from "./Navbar.module.css";
-import {Link} from 'react-router-dom'
+import React from 'react';
+import cls from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
+
+// const setActive = ({isActive}) => isActive ? {cls.link + ' ' + cls.avtive} : cls.link
 
 const Navbar = () => {
   return (
     <nav className={cls.nav}>
       <div className={cls.item}>
-        <Link to='/profile' className={cls.link}>Profile</Link>
-        {/* <a className={cls.link} href="/profile">Profile</a> */}
+        <NavLink
+          to="/profile"
+          className={({ isActive }) =>
+            isActive ? cls.link + ' ' + cls.active : cls.link
+          }
+        >
+          Profile
+        </NavLink>
       </div>
       <div className={cls.item}>
-      <Link to='/dialogs' className={cls.link}>Messages</Link>
-        {/* <a className={cls.link} href="/dialogs">Messages</a> */}
+        <NavLink to="/dialogs" className={({ isActive }) =>
+            isActive ? cls.link + ' ' + cls.active : cls.link
+          }>
+          Messages
+        </NavLink>
       </div>
       <div className={cls.item}>
-        <a className={cls.link}>Mews</a>
+        <NavLink to="" className={({ isActive }) =>
+            isActive ? cls.link + ' ' + cls.active : cls.link
+          }>
+          News
+        </NavLink>
       </div>
       <div className={cls.item}>
-        <a className={cls.link}>Music</a>
+        <NavLink to="" className={({ isActive }) =>
+            isActive ? cls.link + ' ' + cls.active : cls.link
+          }>
+          Music
+        </NavLink>
       </div>
       <div className={cls.item}>
-        <a className={cls.link}>Settings</a>
+        <NavLink to="" className={({ isActive }) =>
+            isActive ? cls.link + ' ' + cls.active : cls.link
+          }>
+          Settings
+        </NavLink>
       </div>
     </nav>
   );
